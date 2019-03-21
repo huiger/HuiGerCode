64fbb3498ae0b78547bb8509f0740a402504d31a
fengdi-huiger <zhihuiemail@163.com>
2019-03-21 18:12:48 +0800

    add  popupwindUtils计算位置

--- library/src/main/java/com/huige/library/popupwind/PopupWindowUtils.java
+++ library/src/main/java/com/huige/library/popupwind/PopupWindowUtils.java
@@ -15,6 +15,7 @@ import android.widget.PopupWindow;
 
 import com.huige.library.R;
 import com.huige.library.interfaces.OnViewResultListener;
+import com.huige.library.utils.DeviceUtils;
 
 /**
  * Author : huiGer
@@ -92,6 +93,39 @@ public class PopupWindowUtils extends PopupWindow {
                 activity.getWindow().setAttributes(params);
             }
         });
+    }
+
+
+    /**
+     * 计算出来的位置，y方向就在anchorView的上面和下面对齐显示，x方向就是与屏幕右边对齐显示
+     * 如果anchorView的位置有变化，就可以适当自己额外加入偏移来修正
+     * @param anchorView  呼出window的view
+     * @param contentView   window的内容布局
+     * @return window显示的左上角的xOff,yOff坐标
+     */
+    public static int[] calculatePopWindowPos(final View anchorView, final View contentView) {
+        final int windowPos[] = new int[2];
+        final int anchorLoc[] = new int[2];
+        // 获取锚点View在屏幕上的左上角坐标位置
+        anchorView.getLocationOnScreen(anchorLoc);
+        final int anchorHeight = anchorView.getHeight();
+        // 获取屏幕的高宽
+        final int screenHeight = DeviceUtils.getWindowHeight(anchorView.getContext());
+        final int screenWidth = DeviceUtils.getWindowWidth(anchorView.getContext());
+        contentView.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+        // 计算contentView的高宽
+        final int windowHeight = contentView.getMeasuredHeight();
+        final int windowWidth = contentView.getMeasuredWidth();
+        // 判断需要向上弹出还是向下弹出显示
+        final boolean isNeedShowUp = (screenHeight - anchorLoc[1] - anchorHeight < windowHeight);
+        if (isNeedShowUp) {
+            windowPos[0] = screenWidth - windowWidth;
+            windowPos[1] = anchorLoc[1] - windowHeight;
+        } else {
+            windowPos[0] = screenWidth - windowWidth;
+            windowPos[1] = anchorLoc[1] + anchorHeight;
+        }
+        return windowPos;
     }
 
     public interface onPopupWindClickListener {
